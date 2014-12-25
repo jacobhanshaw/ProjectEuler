eba0d95e684e0f1a3bdf9e4adb7a140b68c39f56
Jacob Hanshaw <jacobhanshaw0@gmail.com>
2014-12-25 01:36:58 -0600

    17

---
+++ euler17.py
@@ -0,0 +1,43 @@
+
+import math
+
+inputString="""75
+95 64
+17 47 82
+18 35 87 10
+20 04 82 47 65
+19 01 23 75 03 34
+88 02 77 73 07 63 67
+99 65 04 28 06 16 70 92
+41 41 26 56 83 40 80 70 33
+41 48 72 33 47 32 37 16 94 29
+53 71 44 65 25 43 91 52 97 51 14
+70 11 33 28 77 73 17 78 39 68 17 57
+91 71 52 38 17 14 91 43 58 50 27 29 48
+63 66 04 68 89 53 67 30 73 16 69 87 40 31
+04 62 98 27 23 09 70 98 73 93 38 53 60 04 23
+"""
+
+numWidth=2
+spaceWidth=1
+
+def numberAtIndexInLine(line,i):
+    index=i*(numWidth+spaceWidth)
+    return int(line[index:(index+2)])
+
+lines=inputString.splitlines(True)
+
+maxItemsInRow=int(math.ceil(len(lines[len(lines)-1])/(numWidth+spaceWidth)))
+
+done=False
+resultLine=[]
+
+for i in range(0,maxItemsInRow,1):
+    resultLine.append(numberAtIndexInLine(lines[len(lines)-1],i))
+      
+for lineIndex in range(len(lines)-2,-1,-1):
+    numbersInRow=int(math.ceil(len(lines[lineIndex])/(numWidth+spaceWidth)))
+    for i in range(0,numbersInRow,1):
+        resultLine[i]=numberAtIndexInLine(lines[lineIndex],i) + max(resultLine[i],resultLine[(i+1)])
+
+print "Result: ",resultLine[0]
