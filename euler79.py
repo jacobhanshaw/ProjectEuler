76296fd9e01701842b87478134842864fb076a93
Jacob Hanshaw <jacobhanshaw0@gmail.com>
2015-01-10 00:56:51 -0600

    79

---
+++ euler79.py
@@ -0,0 +1,16 @@
+
+
+#manually figured it out
+#marked numbers tied unless number was explicitly said to be after another
+#went through list
+
+#took about 1-2 minutes, less than programming would
+
+7
+3
+1
+6
+2
+8
+9
+0
