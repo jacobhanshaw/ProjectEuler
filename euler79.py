738cad4d434b4e87e117496cdd041a1896a1c14d
Jacob Hanshaw <jacobhanshaw0@gmail.com>
2015-01-10 01:01:27 -0600

    79 future ideas

--- euler79.py
+++ euler79.py
@@ -5,6 +5,17 @@
 #went through list
 
 #took about 1-2 minutes, less than programming would
+#would like to program later
+
+"""
+Could just enter into running array and swap items if a value disagrees
+
+319
+319680
+316980
+etc.
+"""
+
 
 7
 3
