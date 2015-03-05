ed0192983fbebe645ec6b043665f5b8681da6d2a
Jacob Hanshaw <jacobhanshaw0@gmail.com>
2015-03-04 23:58:26 -0600

    Not quite

--- Project Euler/euler2.py
+++ Project Euler/euler2.py
@@ -1,11 +1,35 @@
-result = 0
-term0 = 1
-term1 = 1
+import timeit
 
-while term1 < 4000000:
-    term1 += term0
-    term0 = term1 - term0
-    if term1 % 2 == 0:
-        result += term1
+def method0(maxVal):
+    result = 0
+    term0 = 1
+    term1 = 1
+    while term1 <= maxVal:
+        if term1 % 2 == 0:
+            result += term1
+        term1 += term0
+        term0 = term1 - term0
+            
+    return result
 
-print result
+def method1(maxVal):
+    a = 2
+    b = 8
+    c = 34
+    result = a+b
+    while c < maxVal:
+        result += c
+        a,b,c = b,c,(4 * b + a)
+        
+    return result
+
+execs = 100
+maxVal = 4000000
+
+tm0 = timeit.Timer("method0("+str(maxVal)+")",setup="from __main__ import method0")
+tm1 = timeit.Timer("method1("+str(maxVal)+")",setup="from __main__ import method1")
+
+print "Result:",method0(maxVal)
+print "Method 0:",min(tm0.repeat(number=execs))/execs
+print "Result:",method1(maxVal)
+print "Method 1:",min(tm1.repeat(number=execs))/execs
